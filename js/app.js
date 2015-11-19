1edb07fbeeb651478c4e3e1969d9ae67fe6da24b
Michelle Lee <mlee5394@uw.edu>
2015-11-19 15:36:37 -0800

    lab finished

--- js/app.js
+++ js/app.js
@@ -8,5 +8,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var mouseDown = false;
 
+
+
+    canvas.addEventListener('mousedown', function(evt) {
+        ctx.beginPath();
+        var canvasY = evt.clientY - canvas.offsetTop;
+        var canvasX = evt.clientX - canvas.offsetLeft;
+        ctx.moveTo(canvasX, canvasY);
+        mouseDown = true;
+    });
+
+    canvas.addEventListener('mouseup', function(evt) {
+        var canvasX = evt.clientX - canvas.offsetLeft;
+        var canvasY = evt.clientY - canvas.offsetTop;
+        ctx.lineTo(canvasX, canvasY);
+        ctx.stroke();
+        mouseDown = false;
+    });
+
+    canvas.addEventListener('mousemove', function(evt) {
+        if (mouseDown) {
+            var canvasX = evt.clientX - canvas.offsetLeft;
+            var canvasY = evt.clientY - canvas.offsetTop;
+            ctx.lineTo(canvasX, canvasY);
+            ctx.stroke();
+        }
+
+        var color = document.getElementById('line-color-inp').value;
+        ctx.strokeStyle = color;
+
+        var width = document.getElementById('line-width').value;
+        ctx.lineWidth = width;
+
+        var join = document.getElementById('join-style').value;
+        ctx.lineJoin = join;
+
+        var cap = document.getElementById('end-cap').value;
+        ctx.lineCap = cap;
+    });
 }); //DOMContentLoaded
